d88bb44c142db742d68a65594ee38923551d5eda
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-16 03:59:21 +0400

    Update media.module.ts

--- src/modules/media/media.module.ts
+++ src/modules/media/media.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MediaService } from './media.service';
 import { MediaController } from './media.controller';
 import { Media } from './entities/media.entity';
+import { CloudinaryService } from './cloudinary.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Media])],
   controllers: [MediaController],
-  providers: [MediaService],
-  exports: [MediaService],
+  providers: [MediaService, CloudinaryService],
+  exports: [MediaService, CloudinaryService],
 })
 export class MediaModule {}
