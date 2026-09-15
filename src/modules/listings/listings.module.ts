9b519d8383d79dd7454f36a71a046ba367859ced
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-16 02:15:13 +0400

    Update listings.module.ts

--- src/modules/listings/listings.module.ts
+++ src/modules/listings/listings.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Listing } from './entities/listing.entity';
 import { ListingsService } from './listings.service';
 import { ListingsController } from './listings.controller';
+import { MediaModule } from '../media/media.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Listing]), // 🟢 سجّل Listing هنا
+    MediaModule,
   ],
   controllers: [ListingsController],
   providers: [ListingsService],
