994c0cc2c440d5ddcf0a21935a5d649d7b907547
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-16 04:11:06 +0400

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,33 +1,39 @@
 // src/modules/users/users.controller.ts - النسخة الصحيحة
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+function stripPassword(user: User): Omit<User, 'password'> {
+  const { password, ...rest } = user;
+  return rest;
+}
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async getAll(): Promise<User[]> {
-    return this.usersService.findAll();
+  async getAll(): Promise<Omit<User, 'password'>[]> {
+    const users = await this.usersService.findAll();
+    return users.map(stripPassword);
   }
 
   @Get(':id')
-  async getById(@Param('id') id: number): Promise<User> {
-    return this.usersService.findOne(id);
-  }
-
-  @Post()
-  async create(@Body() userData: Partial<User>): Promise<User> {
-    return this.usersService.create(userData);
+  async getById(@Param('id') id: number): Promise<Omit<User, 'password'>> {
+    const user = await this.usersService.findOne(id);
+    return stripPassword(user);
   }
 
   @Put(':id')
   async update(
     @Param('id') id: number,
     @Body() userData: Partial<User>,
-  ): Promise<User> {
-    return this.usersService.update(id, userData);
+  ): Promise<Omit<User, 'password'>> {
+    const { password, ...safeData } = userData;
+    const user = await this.usersService.update(id, safeData);
+    return stripPassword(user);
   }
 
   @Delete(':id')
