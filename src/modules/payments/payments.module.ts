eb5ad1271afc588e463d02defaa49bc4e1db3ff3
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-16 04:05:44 +0400

    Update payments.module.ts

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './payment.entity';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { ListingsModule } from '../listings/listings.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment]), ListingsModule],
   providers: [PaymentsService],
   controllers: [PaymentsController],
 })
