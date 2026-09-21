d0fbb357eeff1a804074aa608d7eb117346ebb17
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-21 15:27:07 +0400

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,36 +1,50 @@
 // src/modules/users/users.controller.ts - النسخة الصحيحة
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { AdminGuard } from '../auth/admin.guard';
+import { Req, UnauthorizedException } from '@nestjs/common';
+
+function stripPassword(user: User): Omit<User, 'password'> {
+  const { password, ...rest } = user;
+  return rest;
+}
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async getAll(): Promise<User[]> {
-    return this.usersService.findAll();
+  @UseGuards(AdminGuard)
+  async getAll(): Promise<Omit<User, 'password'>[]> {
+    const users = await this.usersService.findAll();
+    return users.map(stripPassword);
   }
 
   @Get(':id')
-  async getById(@Param('id') id: number): Promise<User> {
-    return this.usersService.findOne(id);
-  }
-
-  @Post()
-  async create(@Body() userData: Partial<User>): Promise<User> {
-    return this.usersService.create(userData);
+  async getById(@Param('id') id: number, @Req() req: any): Promise<Omit<User, 'password'>> {
+    if (!req.user?.isAdmin && Number(req.user?.userId) !== Number(id)) throw new UnauthorizedException('Not allowed');
+    const user = await this.usersService.findOne(id);
+    return stripPassword(user);
   }
 
   @Put(':id')
   async update(
     @Param('id') id: number,
     @Body() userData: Partial<User>,
-  ): Promise<User> {
-    return this.usersService.update(id, userData);
+    @Req() req: any,
+  ): Promise<Omit<User, 'password'>> {
+    if (!req.user?.isAdmin && Number(req.user?.userId) !== Number(id)) throw new UnauthorizedException('Not allowed');
+    const { password, isAdmin, ...safeData } = userData;
+    const updateData = req.user?.isAdmin ? { ...safeData, ...(isAdmin !== undefined ? { isAdmin } : {}) } : safeData;
+    const user = await this.usersService.update(id, updateData);
+    return stripPassword(user);
   }
 
   @Delete(':id')
+  @UseGuards(AdminGuard)
   async remove(@Param('id') id: number): Promise<void> {
     return this.usersService.remove(id);
   }
