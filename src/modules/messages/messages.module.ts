cb7e4250a91d4d512916446c397bf9079d1eefbc
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-21 13:42:41 +0400

    Update messages.module.ts

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -1,1 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { Conversation } from './entities/conversation.entity';
+import { Message } from './entities/message.entity';
+import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Conversation, Message]), AuthModule, UsersModule],
+  controllers: [MessagesController],
+  providers: [MessagesService],
+})
+export class MessagesModule {}
