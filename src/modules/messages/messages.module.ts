22a19d7d256d75eddcee29fabed1dab73f56bb2b
mustafadahab106-wq <mustafadahab106@gmail.com>
2026-09-23 00:47:39 +0400

    Register notification entity in messages module

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -4,11 +4,12 @@ import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { Conversation } from './entities/conversation.entity';
 import { Message } from './entities/message.entity';
+import { Notification } from './entities/notification.entity';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Conversation, Message]), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forFeature([Conversation, Message, Notification]), AuthModule, UsersModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
